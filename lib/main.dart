0896b74d811c35bc6901231624677d49b637f779
kazuhitonakayama <firstjin19990101@gmail.com>
2022-06-19 22:57:16 +0900

    insert image on app content area

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ void main() => {
             backgroundColor: Colors.blueGrey[900],
           ),
           backgroundColor: Colors.blueGrey.shade200,
+          body: Image(
+            image: NetworkImage('https://images.unsplash.com/photo-1638913662415-8c5f79b20656?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'),
+          ),
         ),
       ),
     ),
